5df246e2a045e2efb00f53ceea66cdcb29df37b6
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-15 09:13:57 -0800

    add animated frame and social media links

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import './Main.css';
+
+export default function Main() {
+  return (
+    <>
+      <div className="greeting">
+        <h1 className="hi">Hi!</h1>
+        <h4 className="name">My name is</h4>
+        <h2 className="ryan">Ryan Parker.</h2>
+        <h2 className="dev">I&apos;m a creative software developer located in Vancouver, WA.</h2>
+        <h3 className="apps">I develop responsive, intuitive, and scalable apps with React and Express.</h3>
+      </div>
+    </>
+  );
+}
