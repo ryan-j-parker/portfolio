db59dddeb67268e7109dafc57452459b4979bc99
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-19 15:14:31 -0800

    add tech stack

---
+++ src/components/Stack.jsx
@@ -0,0 +1,32 @@
+import './Stack.css';
+
+export default function Stack() {
+  return (
+    <div className="stack">
+      <h1>Current tech stack:</h1>
+      <h2>Frameworks/libraries</h2>
+      <ul>
+        <li>React</li>
+        <li>Express</li>
+        <li>Three.js</li>
+      </ul>
+      <h2>Database</h2>
+      <ul>
+        <li>PostgreSQL</li>
+        <li>Supabase</li>
+      </ul>
+      <h2>Tools</h2>
+      <ul>
+        <li>Beekeeper</li>
+        <li>Discord</li>
+        <li>Gimp</li>
+        <li>Git, GitHub</li>
+        <li>Miro</li>
+        <li>Netlify</li>
+        <li>Postman</li>
+        <li>Slack</li>
+        <li>Zoom</li>
+      </ul>
+    </div>
+  );
+}
