27e730f4598c614cb0fb5c0edb2575e8cb96db0f
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-11-19 23:59:22 -0800

    css

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -6,34 +6,36 @@ export default function Stack() {
       <div className="head">
         <h1>Current tech stack:</h1>
       </div>
-      <div className="frame">
-        <h2>Frameworks/libraries</h2>
-        <ul>
-          <li>React</li>
-          <li>Express</li>
-          <li>Three.js</li>
-        </ul>
-      </div>
-      <div className="db">
-        <h2>Database</h2>
-        <ul>
-          <li>PostgreSQL</li>
-          <li>Supabase</li>
-        </ul>
-      </div>
-      <div className="tools">
-        <h2>Tools</h2>
-        <ul>
-          <li>Beekeeper</li>
-          <li>Discord</li>
-          <li>Gimp</li>
-          <li>Git, GitHub</li>
-          <li>Miro</li>
-          <li>Netlify</li>
-          <li>Postman</li>
-          <li>Slack</li>
-          <li>Zoom</li>
-        </ul>
+      <div className="items">
+        <div className="frame">
+          <h2>Frameworks/libraries</h2>
+          <ul>
+            <li>React</li>
+            <li>Express</li>
+            <li>Three.js</li>
+          </ul>
+        </div>
+        <div className="db">
+          <h2>Database</h2>
+          <ul>
+            <li>PostgreSQL</li>
+            <li>Supabase</li>
+          </ul>
+        </div>
+        <div className="tools">
+          <h2>Tools</h2>
+          <ul>
+            <li>Beekeeper</li>
+            <li>Discord</li>
+            <li>Gimp</li>
+            <li>Git, GitHub</li>
+            <li>Miro</li>
+            <li>Netlify</li>
+            <li>Postman</li>
+            <li>Slack</li>
+            <li>Zoom</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
